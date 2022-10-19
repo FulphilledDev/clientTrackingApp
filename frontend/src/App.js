20654acd2c6c8fce2a258b3172d1594b88fdd77a
Philip Simpson <dev.phits@gmail.com>
2022-10-19 16:39:30 -0700

    Header & Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header';
+import AdminHome from './pages/AdminHome';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      My App
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<AdminHome />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
