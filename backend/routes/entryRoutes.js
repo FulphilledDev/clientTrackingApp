f6d8d3fd6ffd1dc14791a5b0ec089a5ba8829734
Philip Simpson <dev.phits@gmail.com>
2022-10-22 14:00:00 -0700

    Entry Routes

---
+++ backend/routes/entryRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const {
+    getEntries,
+    getEntry,
+    createEntry,
+    updateEntry,
+    deleteEntry
+} = require('../controllers/entryController') 
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(getEntries).post(protect, createEntry)
+router.route('/:id').get(getEntry).delete(protect, deleteEntry).put(protect, updateEntry)
+
+
+module.exports = router
