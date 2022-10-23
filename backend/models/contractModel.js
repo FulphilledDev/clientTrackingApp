b3c3379a3a5715e704e3b778af7cd79165c4c5b6
Philip Simpson <dev.phits@gmail.com>
2022-10-22 18:08:01 -0700

    Update Contract Schema

--- backend/models/contractModel.js
+++ backend/models/contractModel.js
@@ -2,58 +2,69 @@ const mongoose = require('mongoose')
 const userModel = require('./userModel')
 
 const contractSchema = mongoose.Schema({
-    creator: {
+    users: [{
         type: String,
-        required: true,
-        unique: true
-    },
-    partner: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    length: {
-        type: Number,
         required: true
-    },
-    startDate: {
-        type: Number,
-        required: true
-    },
-    completionDate: {
-        type: Number,
-        required: true
-    },
-    paymentInterval: {
-        type: Number,
-        required: true
-    },
-    paymentAmount: {
-        type: Number,
-        required: true
-    },
-    service: {
-        type: String,
-        required: true,
-        enum: ['Nutrition Coaching', 'Mental Performance Coaching', 'Life Performance Coaching']
-    },
-    status: {
-        pending: {
-            type: Boolean,
+    }],
+    contract: [{
+        sender: {
+            type: String,
             required: true,
-            default: false
+            unique: true
         },
-        approved: {
-            type: Boolean,
+        recipient: {
+            type: String,
             required: true,
-            default: false
+            unique: true
         },
-        denied: {
-            type: Boolean,
+        sentAt: {
+            type: Date
+        },
+        status: {
+            pending: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            approved: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            denied: {
+                type: Boolean,
+                required: true,
+                default: false
+            }
+        },
+        service: {
+            type: String,
             required: true,
-            default: false
+            enum: ['Nutrition Coaching', 'Mental Performance Coaching', 'Life Performance Coaching']
+        },
+        length: {
+            type: Number,
+            required: true
+        },
+        startDate: {
+            type: Number,
+            required: true
+        },
+        completionDate: {
+            type: Number,
+            required: true
+        },
+        paymentInterval: {
+            type: Number,
+            required: true
+        },
+        paymentAmount: {
+            type: Number,
+            required: true
         }
-    }
+    }]
+},{
+    timestamps: true,
 })
 
 module.exports = mongoose.model('Contract', contractSchema)
