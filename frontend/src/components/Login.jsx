d52f2e34c6e3e753f139030e0a5ced9380a06dc8
Philip Simpson <dev.phits@gmail.com>
2022-10-28 06:46:12 -0700

    Login / Register Form styles

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
 import { login, reset } from '../features/auth/authSlice'
+import { LockClosedIcon } from '@heroicons/react/20/solid'
 import Spinner from './Spinner'
 
 function Login() {
@@ -57,37 +57,41 @@ function Login() {
 
   return (
     <>
-        <section className='heading'>
-            <p>Please login to your account</p>
-        </section>
-        <section className='form'>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <input 
-                        className='form-control'
-                        type="email"
-                        id='email'
-                        name='email'
-                        value={email} 
-                        onChange={onChange}
-                        placeholder='Enter your email'
-                        required
-                    />
-                    <input 
-                        className='form-control'
-                        type="password"
-                        id='password'
-                        name='password'
-                        value={password} 
-                        onChange={onChange}
-                        placeholder='Enter password'
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-block">Submit</button>
-                </div>
-            </form>
+        <section className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            <div className='w-full max-w-md space-y-8'>
+                <p className='mt-6 text-center text-2xl font-bold tracking-tight text-gray-800'>Please login to your account</p>
+                <form onSubmit={onSubmit} className='mt-8 space-y-6'>
+                    <div className="-space-y-px rounded-md shadow-sm">
+                        <label htmlFor="email" className='sr-only'>Email</label>
+                        <input 
+                            className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                            type="email"
+                            id='email'
+                            name='email'
+                            value={email} 
+                            onChange={onChange}
+                            placeholder='Enter your email'
+                            required
+                        />
+                        <input 
+                            className='relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                            type="password"
+                            id='password'
+                            name='password'
+                            value={password} 
+                            onChange={onChange}
+                            placeholder='Enter password'
+                            required
+                        />
+                    </div>
+                        <button className="group relative flex w-full justify-center rounded-md border border-transparent bg-zinc-900 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                            <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
+                                <LockClosedIcon className="h-5 w-5 text-zinc-500 group-hover:text-zinc-400" aria-hidden="true" />
+                            </span>
+                            Submit
+                        </button>
+                </form>
+            </div>
         </section>
     </>
   )
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -4,6 +4,7 @@ import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
 import { register, reset } from '../features/auth/authSlice'
+import { LockClosedIcon } from '@heroicons/react/20/solid'
 import Spinner from './Spinner'
 
 function Register() {
@@ -65,14 +66,15 @@ function Register() {
 
   return (
     <>
-        <section className='heading'>
-            <p>Please create an account</p>
-        </section>
-        <section className='form'>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
+        <section className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            
+
+        <div className='w-full max-w-md space-y-8'>
+            <p className='mt-6 text-center text-2xl font-bold tracking-tight text-gray-800'>Please create an account</p>
+            <form onSubmit={onSubmit} className='mt-8 space-y-6'>
+                <div className="-space-y-px rounded-md shadow-sm">
                     <input 
-                        className='form-control'
+                        className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                         type="text"
                         id='firstName'
                         name='firstName'
@@ -82,7 +84,7 @@ function Register() {
                         required
                     />
                     <input 
-                        className='form-control'
+                        className='relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                         type="text"
                         id='lastName'
                         name='lastName'
@@ -92,7 +94,7 @@ function Register() {
                         required
                     />
                     <input 
-                        className='form-control'
+                        className='relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                         type="email"
                         id='email'
                         name='email'
@@ -102,7 +104,7 @@ function Register() {
                         required
                     />
                     <input 
-                        className='form-control'
+                        className='relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                         type="password"
                         id='password'
                         name='password'
@@ -112,7 +114,7 @@ function Register() {
                         required
                     />
                     <input 
-                        className='form-control'
+                        className='relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                         type="password"
                         id='password2'
                         name='password2'
@@ -122,10 +124,14 @@ function Register() {
                         required
                     />
                 </div>
-                <div className="form-group">
-                    <button className="btn btn-block">Submit</button>
-                </div>
+                <button className="group relative flex w-full justify-center rounded-md border border-transparent bg-zinc-900 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                    <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
+                        <LockClosedIcon className="h-5 w-5 text-zinc-500 group-hover:text-zinc-400" aria-hidden="true" />
+                    </span>
+                        Submit
+                </button>
             </form>
+        </div>
         </section>
     </>
   )
