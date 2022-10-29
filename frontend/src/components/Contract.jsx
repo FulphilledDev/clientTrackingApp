2860596975a47b95ee40e9ca69dac56806673814
Philip Simpson <dev.phits@gmail.com>
2022-10-29 10:36:32 -0700

    Contract Layout

--- frontend/src/components/Contract.jsx
+++ frontend/src/components/Contract.jsx
@@ -56,7 +56,7 @@ function Contract({contract, isLoading, isError}) {
               />
             </div> */}
 
-            <div className="col-span-6 sm:col-span-4">
+            <div className="col-span-3">
               <label className="block text-sm font-medium text-gray-700">
                 Email
                 {/* 
@@ -74,6 +74,42 @@ function Contract({contract, isLoading, isError}) {
                 value={contract.users.receiver}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
               />
+            </div>
+            <div className="col-span-3 flex-column justify-end">
+              <label className="inline-block text-sm font-medium text-gray-700">
+                  Current Status
+              </label>
+              <div>
+                {contract.status === 'pending' ?
+                    <button
+                      className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2 "
+                    >
+                      Pending
+                    </button>
+                  : null}
+                {contract.status === 'approve' ?
+                    <button
+                      className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
+                    >
+                      Active
+                    </button>
+                  : null}
+                
+                {contract.status === 'deny' ?
+                    <button
+                      className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
+                    >
+                      Denied
+                    </button>
+                  : null}
+                {contract.status === 'terminate' ?
+                    <button
+                        className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
+                      >
+                        Terminated
+                      </button>
+                  : null}
+              </div>
             </div>
 
             <div className="col-span-6">
@@ -163,85 +199,49 @@ function Contract({contract, isLoading, isError}) {
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
               />
             </div>
-            <div className="col-span-6">
-            <label className="inline-block text-sm font-medium text-gray-700">
-                Current Status
-            </label>
-            <div className=''>
-            {contract.status === 'pending' ?
-                <button
-                  className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2 "
-                >
-                  Pending
-                </button>
-              : null}
-            {contract.status === 'approve' ?
-                <button
-                  className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
-                >
-                  Active
-                </button>
-              : null}
-            
-            {contract.status === 'deny' ?
-                <button
-                  className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
-                >
-                  Denied
-                </button>
-              : null}
-            {contract.status === 'terminate' ?
-                <button
-                    className="flex justify-center rounded-md border border-transparent bg-yellow-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"
-                  >
-                    Terminated
-                  </button>
-              : null}
-            </div>
-        </div>
-            
-          </div>
-
         <div className="col-span-6">
-              <label className="block text-sm font-medium text-gray-700">
-                Update Contract Status
-              </label>
-              <div
-                type="text"
-                className="mt-1 flex justify-around w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
-              >
-                <button
-                type="submit"
-                name="status"
-                id="status"
-                value={newStatus}
-                onChange={(e) => setNewStatus(e.target.value)}
-                className="justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2"
-                >
-                Approve
-                </button>
-                <button
-                  type="submit"
-                  name="status"
-                  id="status"
-                  value={newStatus}
-                  onChange={(e) => setNewStatus(e.target.value)}
-                  className="justify-center rounded-md border border-transparent bg-red-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
-                >
-                  Deny
-                </button>
-                <button
-                  type="submit"
-                  name="status"
-                  id="status"
-                  value={newStatus}
-                  onChange={(e) => setNewStatus(e.target.value)}
-                  className="justify-center rounded-md border border-transparent bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
-                >
-                  Terminate
-                </button>
-              </div>
-            </div>
+          <label className="block text-sm font-medium text-gray-700">
+            Update Contract Status
+          </label>
+          <div
+            type="text"
+            className="mt-1 flex justify-around w-full rounded-md border-gray-300 shadow-sm focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm"
+          >
+            <button
+            type="submit"
+            name="status"
+            id="status"
+            value={newStatus}
+            onChange={(e) => setNewStatus(e.target.value)}
+            className="justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2"
+            >
+            Approve
+            </button>
+            <button
+              type="submit"
+              name="status"
+              id="status"
+              value={newStatus}
+              onChange={(e) => setNewStatus(e.target.value)}
+              className="justify-center rounded-md border border-transparent bg-red-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
+            >
+              Deny
+            </button>
+            <button
+              type="submit"
+              name="status"
+              id="status"
+              value={newStatus}
+              onChange={(e) => setNewStatus(e.target.value)}
+              className="justify-center rounded-md border border-transparent bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
+            >
+              Terminate
+            </button>
+          </div>
+        </div>
+      </div>
+
+        
         </div>
         
         
