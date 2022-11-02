3f8f2387cd53671d96588d54febd15e22ca208e6
Philip Simpson <dev.phits@gmail.com>
2022-11-02 03:29:07 -0700

    Deployment setup for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -18,6 +19,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/contracts', require('./routes/contractRoutes'))
 app.use('/api/entries', require('./routes/entryRoutes'))
+
+// Serve Frontend for deployment of app
+if(process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    // Then load index.html in the build folder
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({
+            message: 'Welcome to u2u Contracts API!'
+        })
+    })
+}
 
 app.use(errorHandler)
 
